bb2e6a667f1b91ee3621f2f3bec1eea5bb19c789
Joshua "jag" Ginsberg <jag@flowtheory.net>
2014-07-18 14:34:34 -0400

    Better handling of includes to match the node.js jade compiler.

--- jade_tools/compiler.py
+++ jade_tools/compiler.py
@@ -5,6 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+import re
 import os
 import json
 
@@ -14,9 +15,16 @@ from django.core import urlresolvers
 from django.template import loader, RequestContext
 from django.test import RequestFactory
 
+from pyjade.ext.django.loader import Loader
+from pyjade.ext.django.compiler import Compiler
 from pyjade.utils import process
 
 class DjangoJadeCompiler(object):
+
+    INCLUDE_RE = re.compile(
+        r'^(?P<indent>[\t ]*)include +(?P<included>[^ ]+) *$',
+        re.MULTILINE)
+
     def __init__(self, template_path, url_map):
         self.template_path = template_path
         self.url_map = url_map
@@ -75,14 +83,57 @@ class DjangoJadeCompiler(object):
                        'template_path_base': template_path_base,
                        'jade_template_path': jade_template_path}
 
+    def preprocess_includes(self, template_src):
+        # Can't do finditer() here because the indices of matches change
+        match = self.INCLUDE_RE.search(template_src)
+        while match:
+            base_indent, included_path = match.groups()
+            logger.debug('Found include statement: base indent is "%s" and '
+                         'included path is %s', base_indent, included_path)
+            if os.path.exists(included_path):
+                included_src = open(included_path, 'r').read()
+            elif os.path.exists("%s.jade" % included_path):
+                included_src = open("%s.jade" % included_path, 'r').read()
+            else:
+                raise Exception("Include path doesn't exists")
+            if self.INCLUDE_RE.search(included_src):
+                current_pwd = os.getcwd()
+                os.chdir(os.path.dirname('./'+included_src))
+                included_src = self.preprocess_includes(included_src)
+                os.chdir(current_pwd)
+            template_src = (
+                template_src[0:match.start()] + base_indent +
+                included_src.replace('\n', '\n'+base_indent).rstrip() +
+                template_src[match.end():]
+            )
+            match = self.INCLUDE_RE.search(template_src)
+        return template_src
+
+
     def render_jade_with_json(self, jade_template_path, json_file_path,
                               template_path_base, base_file):
         logger.debug('Working with jade template %s', jade_template_path)
-        tmpl = loader.get_template(jade_template_path)
+        # Change the pwd to the template's location
+        current_pwd = os.getcwd()
+        os.chdir(os.path.join(self.template_path, template_path_base))
+        # Hackery to allow for pre-processing the jade source
+        jade_loader = Loader(
+            ('django.template.loaders.filesystem.Loader',
+             'django.template.loaders.app_directories.Loader'))
+        tmpl_src, display_name = jade_loader.load_template_source(
+            jade_template_path)
+        if self.INCLUDE_RE.search(tmpl_src):
+            tmpl_src = self.preprocess_includes(tmpl_src)
+        origin = loader.make_origin(display_name,
+                                    jade_loader.load_template_source,
+                                    jade_template_path, None)
+        compiled_jade = process(tmpl_src, filename=jade_template_path,
+                                compiler=Compiler)
+        tmpl = loader.get_template_from_string(compiled_jade, origin,
+                                               jade_template_path)
         if settings.DEBUG:
-            logger.debug('Template is: %s',
-                         process(open(os.path.join(self.template_path,
-                                                   jade_template_path)).read()))
+            logger.debug(
+                'Template is: %s', tmpl_src)
         # We need to simulate request middleware but without short-circuiting
         # the response
         request_factory = RequestFactory()
@@ -94,7 +145,10 @@ class DjangoJadeCompiler(object):
         for middleware_method in handler._request_middleware:
             middleware_method(req)
         # Render the template with a RequestContext
-        ctx = RequestContext(req, json.load(open(json_file_path)))
+        ctx = RequestContext(req,
+                             json.load(open(os.path.basename(json_file_path))))
+        # Change the pwd back
+        os.chdir(current_pwd)
         return tmpl.render(ctx)
 
 
